d3eb5872455f20d9c07e8ce9b298ede744886220
davidbickley <86498138+davidbickley@users.noreply.github.com>
2024-01-14 16:54:29 -0600

    Users can send surveys

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    { label: 'Survey Title', name: 'title', noValueError: 'You must provide a survey title.' },
+    { label: "Subject Line", name: "subject", noValueError: 'You must provide a subject line.' },
+    { label: "Email Body", name: "body", noValueError: 'You must provide body content.' },
+    { label: "Recipient List", name: 'recipients', noValueError: 'You must provide a recipient list.' }
+];
--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI,{useNewUrlParser: true });
 
 const app = express();
 
@@ -26,20 +27,19 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
     // Express will serve production assets
     // like main.js or main.css
     app.use(express.static('client/build'));
-    
-    const path = require('path');
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-
 
     // Express will server the index.html file
     // if it does not recognize the route
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 const PORT = process.env.PORT || 5000
